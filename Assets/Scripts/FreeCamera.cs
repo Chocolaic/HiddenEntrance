bb54d51a1b9cd77aa2a4a361a66ca974e63a8462
Lialy <chocolate_liy@163.com>
2022-01-10 03:56:35 +0800

    create project

---
+++ Assets/Scripts/FreeCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FreeCamera : MonoBehaviour
+{
+    [SerializeField] float m_rotSpeed = 2f, m_moveSpeed = 2f;
+    [SerializeField] Transform m_camera;
+    private Rigidbody m_rigidbody;
+
+    private float _rotX, _rotY;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        ResetAngle();
+    }
+
+    private void Update()
+    {
+        _rotX = Mathf.Clamp(_rotX - Input.GetAxis("Mouse Y") * m_rotSpeed, -90f, 90f);
+        _rotY += Input.GetAxis("Mouse X") * m_rotSpeed;
+        m_camera.rotation = Quaternion.Euler(_rotX, _rotY, 0);
+
+        var moveDirection = Vector3.zero;
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+
+        m_rigidbody.velocity = m_camera.TransformDirection(moveDirection.normalized * m_moveSpeed);
+    }
+    private void ResetAngle()
+    {
+        var eulerAngle = m_rigidbody.rotation.eulerAngles;
+        _rotX = eulerAngle.x;
+        _rotY = eulerAngle.y;
+    }
+}
---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] RenderTexture m_renderTexture;
+    [SerializeField] Camera m_camera;
+    private void Awake()
+    {
+        m_renderTexture.width = Screen.width;
+        m_renderTexture.height = Screen.height;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var localPos = transform.InverseTransformPoint(other.transform.position);
+
+        if (localPos.z > 0)
+            SwitchCullMask(true);
+    }
+    private void SwitchCullMask(bool on)
+    {
+        m_camera.cullingMask |= (on ? 1 : 0) << LayerMask.NameToLayer("Observable");
+    }
+}
